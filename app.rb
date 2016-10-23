1272e136f3e2c23fb6f4859fb09b806f9332e3ea
ShotaOnishi <showta.0804@gmail.com>
2016-10-24 02:02:58 +0900

    Create line bot2

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # app.rb
 require 'sinatra'
 require 'line/bot'
+require 'json'
 
 def client
   @client ||= Line::Bot::Client.new { |config|
@@ -9,32 +10,36 @@ def client
   }
 end
 
+get '/' do 
+  "Hello, world"
+end
+
 post '/callback' do
   body = request.body.read
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
-  end
-
-  events = client.parse_events_from(body)
-  events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
-      end
     end
-  }
 
-  "OK"
-end
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+
+    "OK"
+  end
