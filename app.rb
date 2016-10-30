9b37bff96500b385c547ec8f99d44699d9d2e011
ShotaOnishi <showta.0804@gmail.com>
2016-10-30 19:04:41 +0900

    make_db_setting2

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ require 'pg'
 # DB設定ファイルの読み込み
 configure :production do
   ActiveRecord::Base.establish_connection(ENV['DB_URL'])
-  use Rack::Auth::Basic do |username, password|
-    username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-  end
+  # use Rack::Auth::Basic do |username, password|
+  #   username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+  # end
 end
 
 require 'require_all'
