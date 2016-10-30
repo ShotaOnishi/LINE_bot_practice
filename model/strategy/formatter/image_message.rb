c5bed617fbd2124721be5d8eb500fe5a3aabb601
ShotaOnishi <showta.0804@gmail.com>
2016-10-30 23:00:39 +0900

    picture_file

--- model/strategy/formatter/image_message.rb
+++ model/strategy/formatter/image_message.rb
@@ -8,17 +8,21 @@ class ImageMessage
   end
 
   def choice_image
+    # images = [
+    #     'https://pbs.twimg.com/media/B_QHDbSVEAA1adJ.jpg',
+    #     'https://pbs.twimg.com/media/BuHSdwCCAAELvUK.jpg',
+    #     'https://i.ytimg.com/vi/6nQyHeiDHu0/hqdefault.jpg',
+    #     'https://s-media-cache-ak0.pinimg.com/564x/f2/a2/4f/f2a24f58f13823def4053e1ae32f2557.jpg',
+    #     'https://pbs.twimg.com/media/CZQMsB-UYAQ-GJA.jpg',
+    #     'https://ssl-stat.amebame.com/pub/content/8265872137/user/article/194267610976870743/ea20fa0d12335b8b2735d1a768d0932a/cached.jpg',
+    #     'https://s-media-cache-ak0.pinimg.com/236x/f6/5c/8d/f65c8dfee01d6407e9e9cd8298cbeb5d.jpg',
+    #     'https://pbs.twimg.com/media/CMTT-vPUAAErxqJ.png',
+    #     'https://pbs.twimg.com/media/Bc7INKDCQAAp1d8.jpg',
+    #     'https://pbs.twimg.com/media/B5RSK0pCYAA45il.png'
+    # ]
     images = [
-        'https://pbs.twimg.com/media/B_QHDbSVEAA1adJ.jpg',
-        'https://pbs.twimg.com/media/BuHSdwCCAAELvUK.jpg',
-        'https://i.ytimg.com/vi/6nQyHeiDHu0/hqdefault.jpg',
-        'https://s-media-cache-ak0.pinimg.com/564x/f2/a2/4f/f2a24f58f13823def4053e1ae32f2557.jpg',
-        'https://pbs.twimg.com/media/CZQMsB-UYAQ-GJA.jpg',
-        'https://ssl-stat.amebame.com/pub/content/8265872137/user/article/194267610976870743/ea20fa0d12335b8b2735d1a768d0932a/cached.jpg',
-        'https://s-media-cache-ak0.pinimg.com/236x/f6/5c/8d/f65c8dfee01d6407e9e9cd8298cbeb5d.jpg',
-        'https://pbs.twimg.com/media/CMTT-vPUAAErxqJ.png',
-        'https://pbs.twimg.com/media/Bc7INKDCQAAp1d8.jpg',
-        'https://pbs.twimg.com/media/B5RSK0pCYAA45il.png'
+        'http://res.cloudinary.com/hmfnscv52/image/upload/v1473405678/zjglt3ch9img4ukxiwrw.png',
+        'http://res.cloudinary.com/hmfnscv52/image/upload/v1475767645/ncsg57zhmhd0zfgqwpq5.png'
     ]
     images.sample
   end
