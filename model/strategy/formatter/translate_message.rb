6c9f8a11f18ad62d2a889b65b92fec11e5b1a920
ShotaOnishi <showta.0804@gmail.com>
2016-11-03 00:19:58 +0900

    translate

--- model/strategy/formatter/translate_message.rb
+++ model/strategy/formatter/translate_message.rb
@@ -6,7 +6,7 @@ class TranslateMessage
     api_key = ENV['GOOGLE_TRANSLATE_KEY']
     gcloud = Google::Cloud.new
     translate_client = gcloud.translate api_key
-    text = context.event.message['text']
+    text = context.event.message['text'].delete("翻訳")
     target = "en"
     translation = translate_client.translate text, to: target
     {
