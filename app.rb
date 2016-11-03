8c813acd1d8b3e1e5018e126bb8bef60063c0e17
ShotaOnishi <showta.0804@gmail.com>
2016-11-04 00:01:46 +0900

    test

--- app.rb
+++ app.rb
@@ -149,7 +149,7 @@ post '/callback' do
           message = ResponceMessage.new(DefaultMessage.new, event)
         else
           message = ResponceMessage.new(DefaultMessage.new, event)
-          p event['message']["id"]
+          p event['source']["type"]
         end
         case event.type
         when Line::Bot::Event::MessageType::Text
