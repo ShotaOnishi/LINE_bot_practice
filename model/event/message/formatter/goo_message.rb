50bec144c27fb93c176a99888849e61a98d909bd
ShotaOnishi <showta.0804@gmail.com>
2016-11-05 00:55:17 +0900

    test

---
+++ model/event/message/formatter/goo_message.rb
@@ -0,0 +1,19 @@
+
+class DefaultMessage
+  def output_message(context)
+    # encoding: utf-8
+    require 'net/https'
+    require 'json'
+    app_id = ENV['GOO_API_KEY']
+    request_data = {'app_id'=>app_id, "sentence"=>context.value['message']['text']}.to_json
+    header = {'Content-type'=>'application/json'}
+    https = Net::HTTP.new('labs.goo.ne.jp', 443)
+    https.use_ssl=true
+    responce = https.post('/api/morph', request_data, header)
+    puts responce.body
+    {
+      type: "text",
+      text: context.value['message']['text']
+    }
+  end
+end
