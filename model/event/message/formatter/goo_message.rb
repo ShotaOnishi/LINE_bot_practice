af32aef07269189b33318631825ff472b04a0f3f
ShotaOnishi <showta.0804@gmail.com>
2016-11-05 01:21:03 +0900

    test

--- model/event/message/formatter/goo_message.rb
+++ model/event/message/formatter/goo_message.rb
@@ -10,7 +10,7 @@ class GooMessage
     https = Net::HTTP.new('labs.goo.ne.jp', 443)
     https.use_ssl=true
     responce = https.post('/api/entity', request_data, header)
-    puts responce.body["ne-list"]
+    puts responce.body["ne_list"]
     {
       type: "text",
       text: context.value['message']['text']
